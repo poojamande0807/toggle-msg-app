bab5ce322fa83c72ad0fc7531b053f1955ac5f1e
poojamande0807 <mandeakshaykumar@gmail.com>
2024-09-25 18:31:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
 
-function App() {
+const initialState = { isVisible: false };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_VISIBILITY":
+      return { isVisible: !state.isVisible };
+
+    default:
+      return state;
+  }
+};
+
+function ToggleMessage() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => dispatch({ type: "TOGGLE_VISIBILITY" })}>
+        Toggle Message
+      </button>
+
+      {state.isVisible && <p>Hello, World!</p>}
     </div>
   );
 }
 
-export default App;
+export default ToggleMessage;
